dbfad7c8ccec6e44ec4609b35d9c4b24a3d76ddc
fidoman <ericfeibelman@gmail.com>
2018-02-28 19:48:26 -0500

    "test"

--- go.sh
+++ go.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-go get -u github.com/efidoman/xdripgo
+CHECKIN=${1:-""}
+BRANCH=${2:-"master"}
+FILES=${3:-"*.*"}
+
+echo "formatting and building $FILES"
+gofmt -w ./$FILES && go build ./$FILES 
+
+if [ "$CHECKIN" != "" ]; then 
+ echo "Checking in $FILES into $BRANCH"
+ git add $FILES
+ git commit -m "\"${CHECKIN}\""
+ go get -u github.com/efidoman/xdripgo
+fi
+
+
+
