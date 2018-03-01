0ba7ff742e97444bcce0ec42ebc3d847fba9758f
fidoman <ericfeibelman@gmail.com>
2018-02-28 19:49:37 -0500

    "test"

--- go.sh
+++ go.sh
@@ -2,10 +2,11 @@
 
 CHECKIN=${1:-""}
 BRANCH=${2:-"master"}
+SRC=${3:-"*.go"}
 FILES=${3:-"*.*"}
 
-echo "formatting and building $FILES"
-gofmt -w ./$FILES && go build ./$FILES 
+echo "formatting and building $SRC"
+gofmt -w ./$SRC && go build ./$SRC 
 
 if [ "$CHECKIN" != "" ]; then 
  echo "Checking in $FILES into $BRANCH"
