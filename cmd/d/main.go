0dedbeaf9e88dd673f594bd196582cc2417133e3
fidoman <ericfeibelman@gmail.com>
2018-05-14 06:00:23 -0400

    testing g5 tx connection

--- cmd/d/main.go
+++ cmd/d/main.go
@@ -11,6 +11,22 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Print("++++++++++++++++++++++++++++ Calling Handle Notify 3532 ++++++++++++++++++++++++++")
+	err = conn.HandleNotify("f8083532-849e-531c-c594-30f1f86a4ea5", func(data []byte) {
+		log.Print("++++++++++++++++++++++++++++ IN HANDLE NOTIFY 3532 ++++++++++++++++++++++++++")
+		log.Print("+++++ IN HANDLE NOTIFY 3532 - data=", data)
+		//		device.Print(os.Stdout)
+	})
+	log.Print("++++++++++++++++++++++++++++ AFTER HANDLE NOTIFY 3532 ++++++++++++++++++++++++++")
+
+	log.Print("++++++++++++++++++++++++++++ Calling Handle Notify febc ++++++++++++++++++++++++++")
+	err = conn.HandleNotify("0000febc-0000-1000-8000-00805f9b34fb", func(data []byte) {
+		log.Print("++++++++++++++++++++++++++++ IN HANDLE NOTIFY febc ++++++++++++++++++++++++++")
+		log.Print("+++++ IN HANDLE NOTIFY febc - data=", data)
+		//		device.Print(os.Stdout)
+	})
+	log.Print("++++++++++++++++++++++++++++ AFTER HANDLE NOTIFY febc ++++++++++++++++++++++++++")
+
 	adapter, err := conn.GetAdapter()
 	if err != nil {
 		log.Fatal(err)
@@ -37,22 +53,6 @@ func main() {
 	}
 
 	device.Print(os.Stdout)
-
-	log.Print("++++++++++++++++++++++++++++ Calling Handle Notify 3532 ++++++++++++++++++++++++++")
-	err = conn.HandleNotify("f8083532-849e-531c-c594-30f1f86a4ea5", func(data []byte) {
-		log.Print("++++++++++++++++++++++++++++ IN HANDLE NOTIFY 3532 ++++++++++++++++++++++++++")
-		log.Print("+++++ IN HANDLE NOTIFY 3532 - data=", data)
-		device.Print(os.Stdout)
-	})
-	log.Print("++++++++++++++++++++++++++++ AFTER HANDLE NOTIFY 3532 ++++++++++++++++++++++++++")
-
-	log.Print("++++++++++++++++++++++++++++ Calling Handle Notify febc ++++++++++++++++++++++++++")
-	err = conn.HandleNotify("0000febc-0000-1000-8000-00805f9b34fb", func(data []byte) {
-		log.Print("++++++++++++++++++++++++++++ IN HANDLE NOTIFY febc ++++++++++++++++++++++++++")
-		log.Print("+++++ IN HANDLE NOTIFY febc - data=", data)
-		device.Print(os.Stdout)
-	})
-	log.Print("++++++++++++++++++++++++++++ AFTER HANDLE NOTIFY febc ++++++++++++++++++++++++++")
 
 	select {}
 	//service, err := conn.GetService("febc f8083532-849e-531c-c594-30f1f86a4ea5")
