0d56a123cea58470019fe6e57c81a684f9ac446b
fidoman <ericfeibelman@gmail.com>
2018-05-15 12:47:55 -0400

    testing g5 tx connection

--- cmd/d/main.go
+++ cmd/d/main.go
@@ -11,20 +11,30 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Print("++++++++++++++++++++++++++++ Calling Handle Notify 3532 ++++++++++++++++++++++++++")
+	log.Print("+++++++++++++++++++++ Calling Handle Notify 3532 ++++++++++++++++++++++++++")
 	err = conn.HandleNotify("f8083532-849e-531c-c594-30f1f86a4ea5", func(data []byte) {
-		log.Print("++++++++++++++++++++++++++++ IN HANDLE NOTIFY 3532 ++++++++++++++++++++++++++")
+		log.Print("+++++++++++++++++++++ IN HANDLE NOTIFY 3532 ++++++++++++++++++++++++++")
 		log.Print("+++++ IN HANDLE NOTIFY 3532 - data=", data)
 		//		device.Print(os.Stdout)
 	})
-	log.Print("++++++++++++++++++++++++++++ AFTER HANDLE NOTIFY 3532 ++++++++++++++++++++++++++")
+	if err != nil {
+		log.Print("HandleNotify 3532, err = ", err)
+	} else {
+		log.Print("HandleNotify 3532 returned without error")
+	}
+	log.Print("+++++++++++++++++++++ AFTER HANDLE NOTIFY 3532 ++++++++++++++++++++++++++")
 
-	log.Print("++++++++++++++++++++++++++++ Calling Handle Notify febc ++++++++++++++++++++++++++")
+	log.Print("+++++++++++++++++++++ Calling Handle Notify febc ++++++++++++++++++++++++++")
 	err = conn.HandleNotify("0000febc-0000-1000-8000-00805f9b34fb", func(data []byte) {
 		log.Print("++++++++++++++++++++++++++++ IN HANDLE NOTIFY febc ++++++++++++++++++++++++++")
 		log.Print("+++++ IN HANDLE NOTIFY febc - data=", data)
 		//		device.Print(os.Stdout)
 	})
+	if err != nil {
+		log.Print("HandleNotify febc, err = ", err)
+	} else {
+		log.Print("HandleNotify febc returned without error")
+	}
 	log.Print("++++++++++++++++++++++++++++ AFTER HANDLE NOTIFY febc ++++++++++++++++++++++++++")
 
 	adapter, err := conn.GetAdapter()
