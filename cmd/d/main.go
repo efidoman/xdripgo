3302151e5f6903a491bedf9149d05e1d21165bac
fidoman <ericfeibelman@gmail.com>
2018-05-15 17:59:06 -0400

    testing g5 tx connection

--- cmd/d/main.go
+++ cmd/d/main.go
@@ -11,6 +11,33 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	adapter, err := conn.GetAdapter()
+	if err != nil {
+		log.Fatal(err)
+	}
+	device, err := conn.GetDeviceByName("DexcomFE")
+	if err != nil {
+		log.Print("device not in cache yet.")
+	} else {
+		log.Print("device is in cache already - removing from adapter cache")
+		adapter.RemoveDevice(device)
+	}
+
+	log.Print("Discovering ...")
+	//	device, err = conn.Discover(0, "DexcomFE")
+	device, err = conn.Discover(0, "0000febc-0000-1000-8000-00805f9b34fb")
+	if err != nil {
+		log.Print("Still couldn't find device after discovery, err=", err)
+		//		log.Fatal(err)
+	}
+	err = device.Connect()
+	if err != nil {
+		log.Print("could not connect to device")
+	} else {
+		log.Print("connected")
+	}
+
+	device.Print(os.Stdout)
 	log.Print("+++++++++++++++++++++ Calling Handle Notify 3532 ++++++++++++++++++++++++++")
 	err = conn.HandleNotify("f8083532-849e-531c-c594-30f1f86a4ea5", func(data []byte) {
 		log.Print("+++++++++++++++++++++ IN HANDLE NOTIFY 3532 ++++++++++++++++++++++++++")
@@ -37,35 +64,7 @@ func main() {
 	}
 	log.Print("++++++++++++++++++++++++++++ AFTER HANDLE NOTIFY febc ++++++++++++++++++++++++++")
 
-	adapter, err := conn.GetAdapter()
-	if err != nil {
-		log.Fatal(err)
-	}
-	device, err := conn.GetDeviceByName("DexcomFE")
-	if err != nil {
-		log.Print("device not in cache yet.")
-	} else {
-		log.Print("device is in cache already - removing from adapter cache")
-		adapter.RemoveDevice(device)
-	}
-
-	log.Print("Discovering ...")
-	device, err = conn.Discover(0, "DexcomFE")
-	if err != nil {
-		log.Print("Still couldn't find device after discovery")
-		log.Fatal(err)
-	}
-	err = device.Connect()
-	if err != nil {
-		log.Print("could not connect to device")
-	} else {
-		log.Print("connected")
-	}
-
-	device.Print(os.Stdout)
-
 	select {}
-	//service, err := conn.GetService("febc f8083532-849e-531c-c594-30f1f86a4ea5")
 	//device, err = conn.Discover(0, "f8083532-849e-531c-c594-30f1f86a4ea5")
 	//device, err = conn.Discover(0, "f8083532-849e-531c-c594-30f1f86a4ea5")
 }
