95d4942376b0ebf66400974b76da374bc2c5bdf8
fidoman <ericfeibelman@gmail.com>
2018-06-19 08:05:42 -0400

    refactoring work

--- glucose.go
+++ glucose.go
@@ -28,8 +28,30 @@ type Glucose struct {
 	//MBG          float64
 }
 
-func NewGlucose(g messages.GlucoseRxMessage, t messages.TransmitterTimeRxMessage, s messages.SensorRxMessage, syncDate int64, rssi int) Glucose {
-	var glucose Glucose
+func NewGlucose(g messages.GlucoseRxMessage, t messages.TransmitterTimeRxMessage, s messages.SensorRxMessage, syncDate int64, rssi int, name string) Glucose {
+	var gluc Glucose
+	// Entry native
+	gluc.Device = name
+	gluc.Date = int64(g.Timestamp) // probably not Timestamp because I'm not sure it is 1970 based
+	gluc.DateString = "convert this using nightscout api"
+	gluc.SGV = int(g.Glucose)
+	gluc.Direction = "calculate this?"
+	gluc.Type = "sgv"
+	gluc.Filtered = int(s.Filtered)
+	gluc.Unfiltered = int(s.Unfiltered)
+	gluc.RSSI = int(rssi)
+	gluc.Noise = 1     // need to calculate this also
+	gluc.Slope = 1000  // need to use calculated values
+	gluc.Intercept = 0 // need to use calculated values
+	gluc.Scale = 1     // what is this?
+	gluc.MBG = 2       // what is this?
 
-	return glucose
+	// added
+	gluc.SGV = int(g.Glucose)
+	gluc.BGlucose = int32(g.Glucose)
+	gluc.Trend = 2 // where to get
+	gluc.State = SensorStateString(g.State)
+	gluc.Status = TransmitterStatusString(g.Status)
+
+	return gluc
 }
